33b22185eebdfb1518ab80ec30d90b54654cecaa
Nathan Fiedler <nathanfiedler@fastmail.fm>
2017-11-13 18:11:35 -0800

    Fix tests by using supertest
    The request-based approach was not really working, primarily because of the
    failure to use the async support in mocha. But as it turns out, supertest is
    much easier to use for testing Express-based apps anyway, so introduced that
    as well.
    
    npm test passes

--- test/test.js
+++ test/test.js
@@ -1,39 +1,56 @@
 /* eslint-env mocha */
-const assert = require('chai').assert
-const request = require('request')
+const request = require('supertest')
 
 // start the server
-require('../app.js')
+const app = require('../app.js')
 
 //
 // attributes tests (tags, locations, etc)
 //
 describe('Attributes', function () {
   describe('tags', function () {
-    it('should return list of tags in JSON foramt', function () {
-      request.get('http://localhost:' + process.env.PORT + '/api/tags', function (err, res, body) {
-        assert.isNull(err)
-        assert.equal(res.statusCode, 200)
-        assert.equal(body, '{"tag":"cat","count":2},{"tag":"dog","count":5},{"tag":"kitten","count":100},{"tag":"puppy","count":40}')
-      })
+    it('should return list of tags in JSON format', function (done) {
+      request(app)
+        .get('/api/tags')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(/puppy/)
+        .end(function (err, res) {
+          if (err) {
+            return done(err)
+          }
+          done()
+        })
     })
   })
   describe('locations', function () {
-    it('should return list of locations in JSON foramt', function () {
-      request.get('http://localhost:' + process.env.PORT + '/api/locations', function (err, res, body) {
-        assert.isNull(err)
-        assert.equal(res.statusCode, 200)
-        assert.equal(body, '{"location":"dublin","count":6},{"location":"hawaii","count":4},{"location":"piedmont","count":5},{"location":"vancouver","count":7}')
-      })
+    it('should return list of locations in JSON format', function (done) {
+      request(app)
+        .get('/api/locations')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(/hawaii/)
+        .end(function (err, res) {
+          if (err) {
+            return done(err)
+          }
+          done()
+        })
     })
   })
   describe('years', function () {
-    it('should return list of years in JSON foramt', function () {
-      request.get('http://localhost:' + process.env.PORT + '/api/years', function (err, res, body) {
-        assert.isNull(err)
-        assert.equal(res.statusCode, 200)
-        assert.equal(body, '{"year":1986,"count":1},{"year":1992,"count":1},{"year":1994,"count":1},{"year":2002,"count":9}')
-      })
+    it('should return list of years in JSON format', function (done) {
+      request(app)
+        .get('/api/years')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .expect(/1994/)
+        .end(function (err, res) {
+          if (err) {
+            return done(err)
+          }
+          done()
+        })
     })
   })
 })
