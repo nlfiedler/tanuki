ea4adb77de6ba16d2e733874e4266ace4f3df706
Nathan Fiedler <nathanfiedler@fastmail.fm>
2018-05-12 14:59:33 -0700

    Fix the navbar code to match my style

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,7 @@
 //
 // Add Elm to the page, as needed.
 //
+/* global Elm */
 const elmMain = document.getElementById('elm-main')
 if (elmMain) {
   Elm.Main.embed(elmMain)
@@ -8,18 +9,18 @@ if (elmMain) {
 
 document.addEventListener('DOMContentLoaded', function () {
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  var navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'), 0)
   // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+  if (navbarBurgers.length > 0) {
     // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
+    navbarBurgers.forEach(function (el) {
+      el.addEventListener('click', function () {
         // Get the target from the "data-target" attribute
-        var target = $el.dataset.target
-        var $target = document.getElementById(target)
+        var target = document.getElementById(el.dataset.target)
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
+        el.classList.toggle('is-active')
+        target.classList.toggle('is-active')
       })
     })
   }
