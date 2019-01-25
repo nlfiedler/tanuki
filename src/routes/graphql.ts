aedbcf1932107f97a39dafe00dfd6b6baa160d83
Nathan Fiedler <nathanfiedler@fastmail.fm>
2019-01-24 19:56:10 -0800

    Move GraphQL schema to public directory
    Kinda makes more sense there than being in the source directory.
    
    npm test passes

--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -10,8 +10,9 @@ const { makeExecutableSchema } = require('apollo-server')
 
 const router = express.Router()
 
-// assemble the parts into a schema object
-const schemaPath = path.join(__dirname, '..', '..', 'lib', 'schema.graphql')
+// Assemble the parts into a schema object; by using a separate file for the
+// schema and invoking makeExecutableSchema() we get the docs in the playground.
+const schemaPath = path.join('public', 'schema.graphql')
 const typeDefs = fs.readFileSync(schemaPath, 'utf8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
