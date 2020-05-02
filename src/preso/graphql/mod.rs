475964b98450c8a438aa96c3f78785aa7bff7e11
Nathan Fiedler <nathanfiedler@fastmail.fm>
2020-05-02 15:48:48 -0700

    GraphQL query for asset by id and digest
    cargo test passes

--- src/preso/graphql/mod.rs
+++ src/preso/graphql/mod.rs
@@ -3,7 +3,8 @@
 //
 use crate::data::repositories::RecordRepositoryImpl;
 use crate::data::sources::EntityDataSource;
-use crate::domain::entities::LabeledCount;
+use crate::domain::entities::{Asset, LabeledCount};
+use chrono::prelude::*;
 use juniper::{graphql_scalar, FieldResult, ParseScalarResult, ParseScalarValue, RootNode, Value};
 use std::sync::Arc;
 
@@ -62,6 +63,65 @@ graphql_scalar!(BigInt where Scalar = <S> {
     }
 });
 
+#[juniper::object(description = "An `Asset` defines a single entity in the storage system.")]
+impl Asset {
+    /// The unique asset identifier.
+    fn id(&self) -> String {
+        self.key.clone()
+    }
+
+    /// The original filename of the asset when it was imported.
+    fn filename(&self) -> String {
+        self.filename.clone()
+    }
+
+    /// The size in bytes of the asset.
+    fn filesize(&self) -> BigInt {
+        BigInt(self.byte_length as i64)
+    }
+
+    /// The date/time that best represents the asset.
+    fn datetime(&self) -> DateTime<Utc> {
+        if let Some(ud) = self.user_date.as_ref() {
+            ud.clone()
+        } else if let Some(od) = self.original_date.as_ref() {
+            od.clone()
+        } else {
+            self.import_date.clone()
+        }
+    }
+
+    /// The media type (nee MIME type) of the asset.
+    fn mimetype(&self) -> String {
+        self.media_type.clone()
+    }
+
+    /// The list of tags associated with this asset.
+    fn tags(&self) -> Vec<String> {
+        self.tags.clone()
+    }
+
+    /// The date provided by the user.
+    fn userdate(&self) -> Option<DateTime<Utc>> {
+        self.user_date.clone()
+    }
+
+    /// A caption attributed to the asset.
+    fn caption(&self) -> Option<String> {
+        self.caption.clone()
+    }
+
+    /// For video assets, the duration in seconds.
+    fn duration(&self) -> Option<i32> {
+        self.duration.map(|v| v as i32)
+    }
+
+    /// Location information for the asset.
+    fn location(&self) -> Option<String> {
+        self.location.clone()
+    }
+}
+
 #[juniper::object(description = "An attribute name and the number of assets it references.")]
 impl LabeledCount {
     /// Label for an asset attribute, such as a tag or location.
@@ -79,6 +139,18 @@ pub struct QueryRoot;
 
 #[juniper::object(Context = Arc<dyn EntityDataSource>)]
 impl QueryRoot {
+    /// Retrieve an asset by its unique identifier.
+    fn asset(executor: &Executor, id: String) -> FieldResult<Asset> {
+        use crate::domain::usecases::fetch::{FetchAsset, Params};
+        use crate::domain::usecases::UseCase;
+        let source = executor.context().clone();
+        let repo = RecordRepositoryImpl::new(source);
+        let usecase = FetchAsset::new(Box::new(repo));
+        let params = Params::new(id);
+        let asset = usecase.call(params)?;
+        Ok(asset)
+    }
+
     /// Return the total number of assets in the system.
     fn count(executor: &Executor) -> FieldResult<i32> {
         use crate::domain::usecases::count::CountAssets;
@@ -101,6 +173,18 @@ impl QueryRoot {
         let params = NoParams {};
         let locations: Vec<LabeledCount> = usecase.call(params)?;
         Ok(locations)
+    }
+
+    /// Look for an asset by the hash digest (SHA256).
+    fn lookup(executor: &Executor, checksum: String) -> FieldResult<Option<Asset>> {
+        use crate::domain::usecases::checksum::{AssetByChecksum, Params};
+        use crate::domain::usecases::UseCase;
+        let source = executor.context().clone();
+        let repo = RecordRepositoryImpl::new(source);
+        let usecase = AssetByChecksum::new(Box::new(repo));
+        let params = Params::new(checksum);
+        let asset = usecase.call(params)?;
+        Ok(asset)
     }
 
     /// Retrieve the list of tags and their associated asset count.
@@ -145,8 +229,113 @@ mod tests {
     use super::*;
     use crate::data::sources::MockEntityDataSource;
     use failure::err_msg;
-    use juniper::Variables;
-    // use mockall::predicate::*;
+    use juniper::{InputValue, Variables};
+    use mockall::predicate::*;
+
+    #[test]
+    fn test_query_asset_ok() {
+        // arrange
+        let asset1 = Asset {
+            key: "abc123".to_owned(),
+            checksum: "cafebabe".to_owned(),
+            filename: "img_1234.jpg".to_owned(),
+            byte_length: 1048576,
+            media_type: "image/jpeg".to_owned(),
+            tags: vec!["cat".to_owned(), "dog".to_owned()],
+            import_date: Utc.ymd(2018, 5, 31).and_hms(21, 10, 11),
+            caption: None,
+            location: Some("hawaii".to_owned()),
+            duration: None,
+            user_date: None,
+            original_date: None,
+        };
+        let mut mock = MockEntityDataSource::new();
+        mock.expect_get_asset()
+            .with(eq("abc123"))
+            .returning(move |_| Ok(asset1.clone()));
+        let ctx: Arc<dyn EntityDataSource> = Arc::new(mock);
+        // act
+        let schema = create_schema();
+        let query = r#"query Fetch($id: String!) {
+            asset(id: $id) {
+                id filename filesize datetime mimetype
+                tags userdate caption duration location
+            }
+        }"#;
+        let mut vars = Variables::new();
+        vars.insert("id".to_owned(), InputValue::scalar("abc123"));
+        let (res, _errors) = juniper::execute(query, None, &schema, &vars, &ctx).unwrap();
+        // assert
+        let res = res.as_object_value().unwrap();
+        let res = res.get_field_value("asset").unwrap();
+        let object = res.as_object_value().unwrap();
+
+        let res = object.get_field_value("id").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "abc123");
+
+        let res = object.get_field_value("filename").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "img_1234.jpg");
+
+        // filesize is a BigInt that is represented as a String
+        let res = object.get_field_value("filesize").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "1048576");
+
+        // datetime is a DateTime that is represented as a String
+        let res = object.get_field_value("datetime").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(&actual[..19], "2018-05-31T21:10:11");
+
+        let res = object.get_field_value("mimetype").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "image/jpeg");
+
+        let res = object.get_field_value("tags").unwrap();
+        let list_result = res.as_list_value().unwrap();
+        let tags = ["cat", "dog"];
+        for (idx, entry) in list_result.iter().enumerate() {
+            let actual = entry.as_scalar_value::<String>().unwrap();
+            assert_eq!(actual, tags[idx]);
+        }
+
+        let res = object.get_field_value("userdate").unwrap();
+        assert!(res.is_null());
+        let res = object.get_field_value("caption").unwrap();
+        assert!(res.is_null());
+        let res = object.get_field_value("duration").unwrap();
+        assert!(res.is_null());
+
+        let res = object.get_field_value("location").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "hawaii");
+    }
+
+    #[test]
+    fn test_query_asset_err() {
+        // arrange
+        let mut mock = MockEntityDataSource::new();
+        mock.expect_get_asset()
+            .with(eq("abc123"))
+            .returning(|_| Err(err_msg("oh no")));
+        let ctx: Arc<dyn EntityDataSource> = Arc::new(mock);
+        // act
+        let schema = create_schema();
+        let query = r#"query Fetch($id: String!) {
+            asset(id: $id) {
+                id filename filesize datetime mimetype
+                tags userdate caption duration location
+            }
+        }"#;
+        let mut vars = Variables::new();
+        vars.insert("id".to_owned(), InputValue::scalar("abc123"));
+        let (res, errors) = juniper::execute(query, None, &schema, &vars, &ctx).unwrap();
+        // assert
+        assert!(res.is_null());
+        assert_eq!(errors.len(), 1);
+        assert!(errors[0].error().message().contains("oh no"));
+    }
 
     #[test]
     fn test_query_count_ok() {
@@ -251,6 +440,95 @@ mod tests {
         )
         .unwrap();
         // assert
+        assert!(res.is_null());
+        assert_eq!(errors.len(), 1);
+        assert!(errors[0].error().message().contains("oh no"));
+    }
+
+    #[test]
+    fn test_query_lookup_some() {
+        // arrange
+        let asset1 = Asset {
+            key: "abc123".to_owned(),
+            checksum: "cafebabe".to_owned(),
+            filename: "img_1234.jpg".to_owned(),
+            byte_length: 1048576,
+            media_type: "image/jpeg".to_owned(),
+            tags: vec!["cat".to_owned(), "dog".to_owned()],
+            import_date: Utc.ymd(2018, 5, 31).and_hms(21, 10, 11),
+            caption: None,
+            location: Some("hawaii".to_owned()),
+            duration: None,
+            user_date: None,
+            original_date: None,
+        };
+        let mut mock = MockEntityDataSource::new();
+        mock.expect_query_by_checksum()
+            .with(eq("cafebabe"))
+            .returning(move |_| Ok(Some("abc123".to_owned())));
+        mock.expect_get_asset()
+            .with(eq("abc123"))
+            .returning(move |_| Ok(asset1.clone()));
+        let ctx: Arc<dyn EntityDataSource> = Arc::new(mock);
+        // act
+        let schema = create_schema();
+        let query = r#"query Lookup($checksum: String!) {
+            lookup(checksum: $checksum) { id }
+        }"#;
+        let mut vars = Variables::new();
+        vars.insert("checksum".to_owned(), InputValue::scalar("cafebabe"));
+        let (res, _errors) = juniper::execute(query, None, &schema, &vars, &ctx).unwrap();
+        // assert
+        let res = res.as_object_value().unwrap();
+        let res = res.get_field_value("lookup").unwrap();
+        let object = res.as_object_value().unwrap();
+        let res = object.get_field_value("id").unwrap();
+        let actual = res.as_scalar_value::<String>().unwrap();
+        assert_eq!(actual, "abc123");
+    }
+
+    #[test]
+    fn test_query_lookup_none() {
+        // arrange
+        let mut mock = MockEntityDataSource::new();
+        mock.expect_query_by_checksum()
+            .with(eq("cafebabe"))
+            .returning(|_| Ok(None));
+        let ctx: Arc<dyn EntityDataSource> = Arc::new(mock);
+        // act
+        let schema = create_schema();
+        let query = r#"query Lookup($checksum: String!) {
+            lookup(checksum: $checksum) { id }
+        }"#;
+        let mut vars = Variables::new();
+        vars.insert("checksum".to_owned(), InputValue::scalar("cafebabe"));
+        let (res, errors) = juniper::execute(query, None, &schema, &vars, &ctx).unwrap();
+        // assert
+        let res = res.as_object_value().unwrap();
+        let res = res.get_field_value("lookup").unwrap();
+        assert!(res.is_null());
+        assert_eq!(errors.len(), 0);
+    }
+
+    #[test]
+    fn test_query_lookup_err() {
+        // arrange
+        let mut mock = MockEntityDataSource::new();
+        mock.expect_query_by_checksum()
+            .with(eq("cafebabe"))
+            .returning(|_| Err(err_msg("oh no")));
+        let ctx: Arc<dyn EntityDataSource> = Arc::new(mock);
+        // act
+        let schema = create_schema();
+        let query = r#"query Lookup($checksum: String!) {
+            lookup(checksum: $checksum) { id }
+        }"#;
+        let mut vars = Variables::new();
+        vars.insert("checksum".to_owned(), InputValue::scalar("cafebabe"));
+        let (res, errors) = juniper::execute(query, None, &schema, &vars, &ctx).unwrap();
+        // assert
+        let res = res.as_object_value().unwrap();
+        let res = res.get_field_value("lookup").unwrap();
         assert!(res.is_null());
         assert_eq!(errors.len(), 1);
         assert!(errors[0].error().message().contains("oh no"));
