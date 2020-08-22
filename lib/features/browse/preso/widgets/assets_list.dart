c450602d50c9f72a5b078dc60896c4b88e3b9f3c
Nathan Fiedler <nathanfiedler@fastmail.fm>
2020-08-22 16:43:32 -0700

    Show asset thumbnails in a wrapped grid-like display

--- lib/features/browse/preso/widgets/assets_list.dart
+++ lib/features/browse/preso/widgets/assets_list.dart
@@ -3,7 +3,9 @@
 //
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:tanuki/core/domain/entities/search.dart';
+import 'package:tanuki/environment_config.dart';
 import 'package:tanuki/features/browse/preso/bloc/asset_browser_bloc.dart';
 
 class AssetsList extends StatelessWidget {
@@ -17,7 +19,7 @@ class AssetsList extends StatelessWidget {
             return Text('Error: ' + state.message);
           }
           if (state is Loaded) {
-            return buildResultsList(context, state.results.results);
+            return buildThumbnails(state.results.results);
           }
           return Center(child: CircularProgressIndicator());
         },
@@ -26,17 +28,34 @@ class AssetsList extends StatelessWidget {
   }
 }
 
-Widget buildResultsList(BuildContext context, List<SearchResult> results) {
+const thumbnail300 = '/api/thumbnail/300/300/';
+
+Widget buildThumbnails(List<SearchResult> results) {
+  final format = DateFormat.EEEE().add_yMMMMd();
   final elements = List<Widget>.from(
     results.map((e) {
-      return Card(
-        child: ListTile(
-          leading: Icon(Icons.photo),
-          title: Text(e.filename),
-          subtitle: Text('Location: ' + e.location.unwrapOr('(none)')),
+      final uri = '${EnvironmentConfig.base_url}$thumbnail300${e.id}';
+      final dateString = format.format(e.datetime);
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image.network(uri, width: 300.0, height: 300.0),
+            SizedBox(
+              width: 300.0,
+              // try keeping the text in a column, the text will automatically
+              // wrap to fix the available space
+              child: Column(children: [
+                Text(dateString),
+                Text(e.filename),
+              ]),
+            ),
+          ],
         ),
       );
     }),
   );
-  return ListView(children: elements);
+  return SingleChildScrollView(
+    child: Wrap(children: elements),
+  );
 }
