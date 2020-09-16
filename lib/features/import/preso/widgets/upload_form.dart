a48070722c2af9ce7296ea3856f45a5fe154c223
Nathan Fiedler <nathanfiedler@fastmail.fm>
2020-09-15 19:53:19 -0700

    feat: allow drag and drop of assets on upload page
    flutter test passes

--- lib/features/import/preso/widgets/upload_form.dart
+++ lib/features/import/preso/widgets/upload_form.dart
@@ -3,8 +3,10 @@
 //
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
+import 'dart:math' as math;
 import 'dart:typed_data';
 import 'package:file_picker_web/file_picker_web.dart';
+import 'package:flutter_dropzone/flutter_dropzone.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tanuki/container.dart';
@@ -17,11 +19,12 @@ class UploadForm extends StatefulWidget {
 
 class _UploadFormState extends State<UploadForm> {
   List<File> _selectedFiles = [];
+  bool highlightDropZone = false;
 
   void _pickFiles(BuildContext context) async {
     final files = await FilePicker.getMultiFile();
     setState(() {
-      _selectedFiles = files;
+      _selectedFiles.addAll(files);
     });
   }
 
@@ -129,6 +132,7 @@ class _UploadFormState extends State<UploadForm> {
                         child: _buildUploadStatus(context, state),
                       ),
                     ),
+                    _buildDropZone(context),
                     _buildFileList(_selectedFiles, state),
                   ],
                 ),
@@ -145,6 +149,52 @@ class _UploadFormState extends State<UploadForm> {
             ],
           );
         },
+      ),
+    );
+  }
+
+  Widget _buildDropZone(BuildContext context) {
+    final theme = Theme.of(context);
+    final borderColor = highlightDropZone
+        ? theme.colorScheme.secondary
+        : theme.colorScheme.primary;
+    // Instead of a hard-coded size for the drop zone, make it a factor of the
+    // size of the headline text in the current them.
+    final boxHeight = theme.textTheme.headline1.fontSize;
+    return DottedBorder(
+      color: borderColor,
+      strokeWidth: 1.0,
+      gap: 4.0,
+      child: Container(
+        height: boxHeight,
+        padding: EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            Builder(
+              builder: (context) => DropzoneView(
+                operation: DragOperation.copy,
+                cursor: CursorType.grab,
+                onHover: () {
+                  if (!highlightDropZone) {
+                    setState(() => highlightDropZone = true);
+                  }
+                },
+                onLeave: () {
+                  setState(() => highlightDropZone = false);
+                },
+                onDrop: (ev) {
+                  // Even when dropping multiple files, this gets called once
+                  // for each file in the set, so must append to the list.
+                  setState(() {
+                    _selectedFiles.add(ev as File);
+                    highlightDropZone = false;
+                  });
+                },
+              ),
+            ),
+            Center(child: Text('You can drag and drop files here')),
+          ],
+        ),
       ),
     );
   }
@@ -169,4 +219,139 @@ Widget _buildListView(List<dynamic> files) {
       itemCount: files.length,
     ),
   );
+}
+
+class DottedBorder extends StatelessWidget {
+  final Color color;
+  final double strokeWidth;
+  final double gap;
+  final Widget child;
+  final EdgeInsets padding;
+
+  DottedBorder({
+    this.color = Colors.black,
+    this.strokeWidth = 1.0,
+    this.gap = 5.0,
+    this.padding = const EdgeInsets.all(8.0),
+    @required this.child,
+  }) {
+    assert(child != null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(strokeWidth / 2),
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: CustomPaint(
+                painter: _DashRectPainter(
+                  color: color,
+                  strokeWidth: strokeWidth,
+                  gap: gap,
+                ),
+              ),
+            ),
+            Padding(
+              padding: padding,
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DashRectPainter extends CustomPainter {
+  double strokeWidth;
+  Color color;
+  double gap;
+
+  _DashRectPainter({
+    this.strokeWidth = 5.0,
+    this.color = Colors.black,
+    this.gap = 5.0,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint dashedPaint = Paint()
+      ..color = color
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke;
+
+    double x = size.width;
+    double y = size.height;
+
+    Path _topPath = getDashedPath(
+      a: math.Point(0, 0),
+      b: math.Point(x, 0),
+      gap: gap,
+    );
+
+    Path _rightPath = getDashedPath(
+      a: math.Point(x, 0),
+      b: math.Point(x, y),
+      gap: gap,
+    );
+
+    Path _bottomPath = getDashedPath(
+      a: math.Point(0, y),
+      b: math.Point(x, y),
+      gap: gap,
+    );
+
+    Path _leftPath = getDashedPath(
+      a: math.Point(0, 0),
+      b: math.Point(0.001, y),
+      gap: gap,
+    );
+
+    canvas.drawPath(_topPath, dashedPaint);
+    canvas.drawPath(_rightPath, dashedPaint);
+    canvas.drawPath(_bottomPath, dashedPaint);
+    canvas.drawPath(_leftPath, dashedPaint);
+  }
+
+  Path getDashedPath({
+    @required math.Point<double> a,
+    @required math.Point<double> b,
+    @required gap,
+  }) {
+    Size size = Size(b.x - a.x, b.y - a.y);
+    Path path = Path();
+    path.moveTo(a.x, a.y);
+    bool shouldDraw = true;
+    math.Point currentPoint = math.Point(a.x, a.y);
+
+    num radians = math.atan(size.height / size.width);
+
+    num dx = math.cos(radians) * gap < 0
+        ? math.cos(radians) * gap * -1
+        : math.cos(radians) * gap;
+
+    num dy = math.sin(radians) * gap < 0
+        ? math.sin(radians) * gap * -1
+        : math.sin(radians) * gap;
+
+    while (currentPoint.x <= b.x && currentPoint.y <= b.y) {
+      shouldDraw
+          ? path.lineTo(currentPoint.x, currentPoint.y)
+          : path.moveTo(currentPoint.x, currentPoint.y);
+      shouldDraw = !shouldDraw;
+      currentPoint = math.Point(
+        currentPoint.x + dx,
+        currentPoint.y + dy,
+      );
+    }
+    return path;
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
 }
